6e33aeffa1e1dc1575d0625e3e80e61b26e8d00e
quay189 <34814999+quay189@users.noreply.github.com>
2020-06-24 10:14:42 +0700

    Create jsconfig

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import './App.scss';
-import Header from './components/Header';
-import NotFound from './components/NotFound';
+import React, { Suspense } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 
 // Lazy load - Code splitting
-const Photo = React.lazy(() => import('./features/Photo'));
+const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
   return (
