38427b1fd59c8e83c287cc180e0c24a21580bf78
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-27 20:56:29 +0700

    fetch API by axios & import to productList

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React, { Suspense } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
+import productApi from "api/productApi";
 
 // Lazy load - Code splitting
 const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
-  return (
-    <div className="photo-app">
-      <Suspense fallback={<div>Loading ...</div>}>
-        <BrowserRouter>
-          <Header />
+	const [productList, setProductList] = useState([]);
+	useEffect(() => {
+		const fetchProductList = async () => {
+			try {
+				const params = {
+					_page: 1,
+					_limit: 10,
+				};
+				const response = await productApi.getAll(params);
+				setProductList(response.data);
+			} catch (error) {
+				console.log("failed to fetch product list:", error);
+			}
+		};
 
-          <Switch>
-            <Redirect exact from="/" to="/photos" />
+		fetchProductList();
+	}, []);
+	return (
+		<div className="photo-app">
+			<Suspense fallback={<div>Loading ...</div>}>
+				<BrowserRouter>
+					<Header />
 
-            <Route path="/photos" component={Photo} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </Suspense>
-    </div>
-  );
+					<Switch>
+						<Redirect exact from="/" to="/photos" />
+
+						<Route path="/photos" component={Photo} />
+						<Route component={NotFound} />
+					</Switch>
+				</BrowserRouter>
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
