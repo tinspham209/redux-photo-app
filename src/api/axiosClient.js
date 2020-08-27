ccc31f37dfd542ddaa7effd151d463886d2a92e9
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-27 22:37:53 +0700

    Handling race condition when using Firebase Token

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,8 +2,43 @@ import axios from "axios";
 import queryString from "query-string";
 import firebase from "firebase";
 
-// Set up default config for http requests here
+const getFirebaseToken = async () => {
+	const currentUser = firebase.auth().currentUser;
+	if (currentUser) {
+		return await currentUser.getIdToken();
+	}
 
+	// Not logged in
+	const hasRememberedAccount = localStorage.getItem(
+		"firebaseui::rememberedAccounts"
+	);
+	if (!hasRememberedAccount) {
+		return null;
+	}
+
+	// Logged in but current user is not fetched ~> wait (10s)
+	return new Promise((resolve, reject) => {
+		const waitTimer = setTimeout(() => {
+			reject(null);
+		}, 10000);
+
+		const unregisterAuthObserver = firebase
+			.auth()
+			.onAuthStateChanged(async (user) => {
+				if (!user) {
+					reject(null);
+				}
+				const token = await user.getIdToken();
+				console.log("[AXIOS] Logged in user: ", token);
+				resolve(token);
+
+				unregisterAuthObserver();
+				clearTimeout(waitTimer);
+			});
+	});
+};
+
+// Set up default config for http requests here
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
 const baseUrl = "https://js-post-api.herokuapp.com/api";
 const axiosClient = axios.create({
@@ -15,9 +50,8 @@ const axiosClient = axios.create({
 });
 axiosClient.interceptors.request.use(async (config) => {
 	// Handle token here ...
-	const currentUser = firebase.auth().currentUser;
-	if (currentUser) {
-		const token = await currentUser.getIdToken();
+	const token = await getFirebaseToken();
+	if (token) {
 		config.headers.Authorization = `Bearer ${token}`;
 	}
 	return config;
